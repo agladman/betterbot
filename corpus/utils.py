249dc85def6f133a23c315c8f7a1fea3de7b5e40
Anthony Gladman <anthony.gladman@gmail.com>
2017-01-13 17:37:30 +0000

    added fetch_image function

--- corpus/utils.py
+++ corpus/utils.py
@@ -1,6 +1,12 @@
 import markovify
 import nltk
+import os
+from random import randint
 import re
+import requests
+import uuid
+
+from betterbot.settings import STATICFILES_DIRS
 
 
 class POSifiedText(markovify.Text):
@@ -12,3 +18,18 @@ class POSifiedText(markovify.Text):
     def word_join(self, words):
         sentence = " ".join(word.split("::")[0] for word in words)
         return sentence
+
+
+def fetch_image():
+    image_filename = 'media/{0}.jpg'.format(uuid.uuid4().hex)
+    r = requests.get(f'https://source.unsplash.com/category/technology/800x600?sig={randint(1, 999)}', stream=True)
+    if r.status_code == 200:
+        try:
+            if int(r.headers['content-length']) < 3145728:
+                with open(os.path.join(STATICFILES_DIRS[0], image_filename), 'wb') as f:
+                    for chunk in r.iter_content(1024):
+                        f.write(chunk)
+                return True
+        except KeyError:
+            pass
+    return False
